1574786d6c31e485f998a82d8755614ee2ebf03a
louise gentillet <louise.gentillet@gmail.com>
2017-12-19 10:12:28 +0100

    Avec filtre géolocalisé
    on a 37 stations autour d'esquirol (rayon de 750m), je cherche maintenant pour trier sur le nombe de vélo dispo

---
+++ stationsVelib.py
@@ -0,0 +1,7 @@
+import requests
+import json
+
+r=requests.get("https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=velo-toulouse&lang=fr&geofilter.distance=43.600470%2C+1.445672%2C+750")
+print(r.content)
+
+# API KEY = 7a6f8095c87666e94af38a52189bdf19fd65ecfb
